79971cbf4a606e12574cef2a2e4c441780ebb359
ppworks <koshikawa@ppworks.jp>
2021-11-17 18:24:50 +0900

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,9 @@
 # homebrew
-set -x PATH /opt/homebrew/bin $PATH
+fish_add_path /opt/homebrew/bin
 
 # bundler
-set -x BUNDLE_WITHOUT production:staging
-set -x BUNDLE_GEM__COC false
-set -x BUNDLE_GEM__MIT false
-set -x BUNDLE_GEM__TEST rspec
-set -x BUNDLE_BUILD__NOKOGIRI "--use-system-libraries=true --with-xml2-include=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/usr/include/libxml2"
-set -x BUNDLE_BUILD__EVENTMACHINE "--with-cppflags=-I/usr/local/opt/openssl/include"
-set -x BUNDLE_BUILD__LIBV8 --with-system-v8
 set -x BUNDLE_JOBS 32
 set -x BUNDLE_RETRY 3
-set -x LDFLAGS "-L/usr/local/opt/libffi/lib"
-set -x PKG_CONFIG_PATH "/usr/local/opt/libffi/lib/pkgconfig"
 
 # goenv
 set -x GOENV_ROOT $HOME/.goenv
@@ -21,8 +12,9 @@ set -x GOPATH $HOME/go
 set -x PATH $GOPATH/bin:$PATH
 status --is-interactive; and source (goenv init -|psub)
 
-# nodebrew
-set -x PATH $HOME/.nodebrew/current/bin:$PATH
+# nodenv
+fish_add_path $HOME/.nodenv/bin
+eval (nodenv init - | source)
 
 # pyenv
 status is-login; and pyenv init --path | source
@@ -42,8 +34,8 @@ function fish_prompt
 end
 
 # development
-set -x PATH $HOME/bin:$PATH
-set -x PATH ./bin:$PATH
+fish_add_path $HOME/bin
+fish_add_path ./bin
 
 # alias
 alias ls='ls -G'
@@ -52,4 +44,3 @@ alias vi='/usr/bin/vim'
 alias be='bundle exec'
 alias diff='colordiff --side-by-side --suppress-common-lines'
 alias less='less -R'
-
